f7e2d8cd95acb2b3dd22e018739a4548c130cc90
Igor Slabykh <igor.slabykh@gmail.com>
2020-01-01 23:27:32 -0500

    created 3 separated json files. All info, names+borders, names+translations

--- db/dataBorders.js
+++ db/dataBorders.js
@@ -1,15 +1,15 @@
 const axios = require("axios");
 const fs = require("fs");
 axios.get(
-    "https://restcountries.eu/rest/v2/all"
+    "https://restcountries.eu/rest/v2/all?fields=name;borders"
 )
     .then(response => {
         console.log(response.data);
         let stringified = JSON.stringify(response.data);
 
-        fs.writeFile("./countries.json", stringified, "utf8", err => {
+        fs.writeFile("./borders.json", stringified, "utf8", err => {
             if (err) {
-                console.error("Unsuccesfully all" + err);
+                console.error("Unsuccesfully borders" + err);
             }
         });
     });
--- db/dataTranslations.js
+++ db/dataTranslations.js
@@ -1,16 +1,16 @@
+
 const axios = require("axios");
 const fs = require("fs");
 axios.get(
-    "https://restcountries.eu/rest/v2/all"
+    "https://restcountries.eu/rest/v2/all?fields=name;translations"
 )
     .then(response => {
         console.log(response.data);
         let stringified = JSON.stringify(response.data);
 
-        fs.writeFile("./countries.json", stringified, "utf8", err => {
+        fs.writeFile("./translations.json", stringified, "utf8", err => {
             if (err) {
-                console.error("Unsuccesfully all" + err);
+                console.error("Unsuccesfully translations" + err);
             }
         });
     });
-
