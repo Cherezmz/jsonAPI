4dade78aff1d2682349d2bb8ddf67b31fe02231e
Igor Slabykh <igor.slabykh@gmail.com>
2020-01-02 16:09:21 -0500

    changed seed file according to model

--- db/seed.js
+++ db/seed.js
@@ -9,9 +9,20 @@ const data = require('../db/countries.json')
 
 const countryData = data.map(item => {
     const country = {}
-    country.name = item.name
-    country.borders = item.borders
-    country.translations = item.translations
+    country.name = item.name,
+        country.topLevelDomain = item.topLevelDomain,
+        country.callingCodes = item.callingCodes,
+        country.capital = item.capital,
+        country.region = item.region,
+        country.subregion = item.subregion,
+        country.population = item.population,
+        country.area = item.area,
+        country.timezones = item.timezones,
+        country.borders = item.borders,
+        country.currencies = item.currencies,
+        country.languages = item.languages,
+        country.translations = item.translations,
+        country.flag = item.flag
     return country
 })
 //console.log(countryData)
--- models/country.js
+++ models/country.js
@@ -2,8 +2,20 @@ const mongoose = require("../db/connection");
 const Schema = mongoose.Schema;
 
 const NameSchema = new Schema({
-    name: String
-
+    name: String,
+    topLevelDomain: Array,
+    callingCodes: Array,
+    capital: String,
+    region: String,
+    subregion: String,
+    population: Number,
+    area: Number,
+    timezones: Array,
+    borders: Array,
+    currencies: Array,
+    languages: Array,
+    translations: Array,
+    flag: String,
 });
 
 module.exports = mongoose.model("Name", NameSchema) 
