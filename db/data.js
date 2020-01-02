fb3e364f0338409e313afddee5b0414ea5feb752
Igor Slabykh <igor.slabykh@gmail.com>
2020-01-01 23:13:00 -0500

    change the ammount of the information: from all to name, borders and translations

--- db/data.js
+++ db/data.js
@@ -1,18 +1,15 @@
 const axios = require("axios");
 const fs = require("fs");
-axios
-    .get(
-        "https://restcountries.eu/rest/v2/all"
-    )
+axios.get(
+    "https://restcountries.eu/rest/v2/all?fields=name;borders;translations"
+)
     .then(response => {
         console.log(response.data);
         let stringified = JSON.stringify(response.data);
 
-        fs.writeFile(__dirname + "/countries.json", stringified, "utf8", err => {
+        fs.writeFile("./countries.json", stringified, "utf8", err => {
             if (err) {
                 console.error("Unsuccesfully" + err);
-            } else {
-                console.log("Successful request!");
             }
         });
     });
