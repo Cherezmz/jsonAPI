48c88c1d74fa55ed6f5bbfab3b8657ab2e505afb
Igor Slabykh <igor.slabykh@gmail.com>
2020-01-05 19:55:41 -0500

    add proxy to package.json

--- frontend/countries/src/App.js
+++ frontend/countries/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import SearchCountry from "./components/SearchCountry"
+import SearchCapital from "./components/SearchCapital"
+import CreateCountry from "./components/CreateCountry"
 
 function App() {
   return (
     <div className="App">
 
-
-
+      <section className="3buttons">
+        <SearchCountry />
+        <SearchCapital />
+        <CreateCountry />
+      </section>
 
     </div>
   );
